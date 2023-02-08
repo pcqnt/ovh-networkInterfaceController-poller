e960283971b9e550848b733108ad7d42f8ea661c
Olivier Picquenot <pcqnt@users.noreply.github.com>
2023-02-08 11:09:56 +0100

    Update get-ovh-networkInterfaceController.py

--- get-ovh-networkInterfaceController.py
+++ get-ovh-networkInterfaceController.py
@@ -8,12 +8,12 @@ from os import environ
 import logging
 
 def main():
-    mrtg_types=['traffic:download','traffic:upload', 
+    mrtg_types=['traffic:upload', 'traffic:download', 
         'errors:upload', 'errors:download', 
         'packets:upload','packets:download']
     logging.basicConfig(level=logging.DEBUG)
 
-    client = ovh.Client(
+    client_ovh = ovh.Client(
         endpoint='ovh-eu',               # Endpoint of API OVH Europe (List of available endpoints)
         application_key=environ['OVH_APP_KEY'],    # Application Key
         application_secret=environ['OVH_APP_SECRET'], # Application Secret
@@ -31,19 +31,20 @@ def main():
         timestamp: int
         value: float
 
-    result = client.get('/dedicated/server')
+
+    result = client_ovh.get('/dedicated/server')
     all_servers=[]
     for server in result:
         url= '/dedicated/server/'+server+'/networkInterfaceController'
         try: 
-            list_of_macs=client.get(url)
+            list_of_macs=client_ovh.get(url)
         except:
             logging.warning('API Error:'+ url)
         all_mac_details=[]
         for mac in list_of_macs:
             url= '/dedicated/server/'+server+'/networkInterfaceController/'+mac
             try:
-                mac_details=client.get(url)
+                mac_details=client_ovh.get(url)
             except:
                 logging.warning('API Error :'+ url)
             all_mac_details.append(mac_details)
@@ -56,23 +57,24 @@ def main():
                 mac = interface['mac']
                 url= "/dedicated/server/"+server.name+"/networkInterfaceController/"+mac+"/mrtg"
                 try:
-                    result2 = client.get(url, period="hourly", type=i,)
+                    result2 = client_ovh.get(url, period="hourly", type=i,)
                 except:
                     logging.warning('API Error (this can be caused by a disconnected interface on the server): '+url+' '+str(interface))
                     sleep(1)
                     continue
+                sleep(1)
                 for j in result2:
                     try:
                         value= float(j['value']['value'])
                     except:
-                        logging.warning('Error converting value to float'+str(value))
+                        logging.warning('Error converting value to float:"'+str(value)+'"')
                     else:
                         result_list.append( MEASUREMENT( server=server.name, mac=mac,
                             timestamp= int(j['timestamp']),
                             value= float(j['value']['value']),
                             linktype=interface['linkType'] ))
-        with InfluxDBClient.from_config_file("config.toml") as client:
-            with client.write_api() as writer:
+        with InfluxDBClient.from_config_file("config.toml") as client_influx:
+            with client_influx.write_api() as writer:
                 logging.info('writing length:'+str(len(result_list)))
                 writer.write(
                     bucket='network-poll',
