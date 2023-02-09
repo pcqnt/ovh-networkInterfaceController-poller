89c9c4555c7e4958a250218997d12da33d3422ad
Olivier Picquenot <pcqnt@users.noreply.github.com>
2023-02-09 09:48:32 +0100

    Update get-ovh-networkInterfaceController.py

--- get-ovh-networkInterfaceController.py
+++ get-ovh-networkInterfaceController.py
@@ -1,41 +1,31 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 import ovh
-from time import sleep
 from influxdb_client import InfluxDBClient
 from influxdb_client.domain.write_precision import WritePrecision
 from dataclasses import dataclass
 from os import environ
 import logging
 
-def main():
-    mrtg_types=['traffic:upload', 'traffic:download', 
-        'errors:upload', 'errors:download', 
-        'packets:upload','packets:download']
-    #logging.basicConfig(level=logging.DEBUG)
+@dataclass
+class INTERFACE:
+    servername: str
+    linkType: str
+    mac: str
+    virtualNetworkInterface: str
+@dataclass
+class MEASUREMENT:
+    server: str
+    linkType: str
+    mac: str
+    timestamp: int
+    value: float
+    measurement_type: str
 
-    client_ovh = ovh.Client(
-        endpoint='ovh-eu',               # Endpoint of API OVH Europe (List of available endpoints)
-        application_key=environ['OVH_APP_KEY'],    # Application Key
-        application_secret=environ['OVH_APP_SECRET'], # Application Secret
-        consumer_key=environ['OVH_CONSUMER_KEY'],       # Consumer Key
-    )
-    @dataclass
-    class INTERFACE:
-        servername: str
-        detail: dict
-    @dataclass
-    class MEASUREMENT:
-        server: str
-        linktype: str
-        mac: str
-        timestamp: int
-        value: float
-        measurement_type: str
-
-    result = client_ovh.get('/dedicated/server')
+def get_all_interfaces(client_ovh):
+    all_my_servers = client_ovh.get('/dedicated/server')
     all_interfaces=[]
-    for server in result:
+    for server in all_my_servers:
         url= '/dedicated/server/'+server+'/networkInterfaceController'
         try: 
             list_of_macs=client_ovh.get(url)
@@ -49,29 +39,54 @@ def main():
                 except:
                     logging.warning('API Error :'+url)
                 else:
-                    all_interfaces.append(INTERFACE(servername=server,detail=mac_details))
+                    all_interfaces.append(INTERFACE(servername=server,
+                        linkType=mac_details['linkType'],
+                        mac=mac_details['mac'],
+                        virtualNetworkInterface=mac_details['virtualNetworkInterface']))
+    return all_interfaces
+
+def get_all_metrics(client_ovh, interfaces_to_poll, mrtg_type):
     result_list=[]
-    for i in mrtg_types:
-        for interface in all_interfaces:
-            mac = interface.detail['mac']
-            url= '/dedicated/server/'+interface.servername+'/networkInterfaceController/'+mac+'/mrtg'
-            try:
-                result2 = client_ovh.get(url, period='hourly', type=i,)
-            except:
-                logging.warning('API Error (this can be caused by a disconnected interface on the server): '+url+' '+str(interface))
-            else:
-                for j in result2:
-                    try:
-                        value= float(j['value']['value'])
-                    except:
-                        logging.warning('Error converting value to float:"'+str(value)+'"')
-                    else:
-                        result_list.append( MEASUREMENT( server=interface.servername, 
-                            mac=mac,
-                            timestamp= int(j['timestamp']),
-                            value= float(j['value']['value']),
-                            linktype=interface.detail['linkType'],
-                            measurement_type=i ))
+    for interface in interfaces_to_poll:
+        url= '/dedicated/server/'+interface.servername+'/networkInterfaceController/'+interface.mac+'/mrtg'
+        try:
+            result = client_ovh.get(url, period='hourly', type=mrtg_type,)
+        except:
+            logging.warning('API Error (this can be caused by a disconnected interface on the server): '+url+' '+str(interface))
+        else:
+            for point in result:
+                try:
+                    value= float(point['value']['value'])
+                except:
+                    logging.warning('Error converting value to float:"'+str(point['value']['value'])+'"')
+                else:
+                    result_list.append( MEASUREMENT( server=interface.servername, 
+                        mac=interface.mac,
+                        timestamp= int(point['timestamp']),
+                        value= float(point['value']['value']),
+                        linkType=interface.linkType,
+                        measurement_type=mrtg_type ))
+    return result_list
+    
+
+def main():
+    #logging.basicConfig(level=logging.DEBUG)
+    client_ovh = ovh.Client(
+        endpoint='ovh-eu',               # Endpoint of API OVH Europe (List of available endpoints)
+        application_key=environ['OVH_APP_KEY'],    # Application Key
+        application_secret=environ['OVH_APP_SECRET'], # Application Secret
+        consumer_key=environ['OVH_CONSUMER_KEY'],       # Consumer Key
+    )
+    all_mrtg_types=['traffic:upload', 'traffic:download', 
+        'errors:upload', 'errors:download', 
+        'packets:upload','packets:download']
+
+    all_interfaces=get_all_interfaces(client_ovh)
+    result_list=[]
+
+    for mrtg_type in all_mrtg_types:
+        result_list.append(get_all_metrics(client_ovh, all_interfaces, mrtg_type))
+   
     with InfluxDBClient.from_config_file("config.toml") as client_influx:
         with client_influx.write_api() as writer:
             logging.info('writing length:'+str(len(result_list)))
@@ -79,7 +94,7 @@ def main():
                 bucket='network-poll',
                 record=result_list,
                 record_measurement_key='server',
-                record_tag_keys=['mac','linktype','measurement_type'],
+                record_tag_keys=['mac','linkType','measurement_type'],
                 record_field_keys=['value'],
                 record_time_key='timestamp',
                 write_precision=WritePrecision.S)
